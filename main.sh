d3d18899c702836b7c2dd7ba7eb7a7ce31787227
Max <15195594+maexbower@users.noreply.github.com>
2024-10-05 14:49:52 +0200

    Fix charakter escaping and add verbosity mode

--- main.sh
+++ main.sh
@@ -13,25 +13,42 @@ gtn_apikey="${GTN_API_KEY:-123456789}" # Use only with "api" auth
 
 gtn_authstring=""
 
+if [ "$VERBOSE" ] && [ "$VERBOSE" ge 1 ]; then
+    echo "GTN Auth Method set to: $gtn_auth"
+fi
+
 # create auth string for CURL requests
 if [ "$gtn_auth" = "basic" ]; then
-    gtn_authstring="-u $gtn_username -p $gtn_password"
+    gtn_authstring='-u "'$gtn_username':'$gtn_password'"'
+    if [ "$VERBOSE" ] && [ "$VERBOSE" ge 2 ]; then
+        echo "authstring set to: $gtn_authstring"
+    fi
 elif [ "$gtn_auth" = "api" ]; then
-    gtn_authstring='-H "X-API-Key: $gtn_apikey"'
+    gtn_authstring='-H "X-API-Key: '$gtn_apikey'"'
+    if [ "$VERBOSE" ] && [ "$VERBOSE" ge 2 ]; then
+        echo "authstring set to: $gtn_authstring"
+    fi
 else
     echo "Authentication Method for GlueTun set to an unknown parameter: $gtn_auth"
     exit 1
 fi
 
-port_number=$(curl --fail --silent --show-error $gtn_authstring $GTN_ADDR/v1/openvpn/portforwarded | jq '.port')
+port_number=$(curl --fail --silent --show-error $gtn_authstring $gtn_addr/v1/openvpn/portforwarded | jq '.port')
 if [ ! "$port_number" ] || [ "$port_number" = "0" ]; then
     echo "Could not get current forwarded port from gluetun, exiting..."
     exit 1
+fi
+if [ "$VERBOSE" ] && [ "$VERBOSE" ge 1 ]; then
+    echo "Port Feedback from GTN was $port_number"
 fi
 
 curl --fail --silent --show-error --cookie-jar /tmp/cookies.txt --cookie /tmp/cookies.txt --header "Referer: $qbt_addr" --data "username=$qbt_username" --data "password=$qbt_password" $qbt_addr/api/v2/auth/login 1> /dev/null
 
 listen_port=$(curl --fail --silent --show-error --cookie-jar /tmp/cookies.txt --cookie /tmp/cookies.txt $qbt_addr/api/v2/app/preferences | jq '.listen_port')
+
+if [ "$VERBOSE" ] && [ "$VERBOSE" ge 1 ]; then
+    echo "Current QBT listening port is: $listen_port"
+fi
 
 if [ ! "$listen_port" ]; then
     echo "Could not get current listen port, exiting..."
