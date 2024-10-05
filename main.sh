5b4c1a5cc5b9c17791df04e1f5ed89441d7ea9fe
Max <15195594+maexbower@users.noreply.github.com>
2024-10-05 14:09:49 +0200

    Add Gluetun Control Server Authentication

--- main.sh
+++ main.sh
@@ -5,8 +5,25 @@ qbt_username="${QBT_USERNAME:-admin}"
 qbt_password="${QBT_PASSWORD:-adminadmin}"
 qbt_addr="${QBT_ADDR:-http://localhost:8080}" # ex. http://10.0.1.48:8080
 gtn_addr="${GTN_ADDR:-http://localhost:8000}" # ex. http://10.0.1.48:8000
+# Gluetun Control Server auth is mandatory starting from Version 3.40.0
+gtn_auth="basic" # Possible auth methods are "basic" and "api"
+gtn_username="${GTN_USERNAME:-admin}" # Use only with "basic" auth
+gtn_password="${GTN_PASSWORD:-admin}" # Use only with "basic" auth
+gtn_apikey="${GTN_API_KEY:-123456789}" # Use only with "api" auth
 
-port_number=$(curl --fail --silent --show-error  $GTN_ADDR/v1/openvpn/portforwarded | jq '.port')
+gtn_authstring=""
+
+# create auth string for CURL requests
+if [ "$gtn_auth" = "basic" ]; then
+    gtn_authstring="-u $gtn_username -p $gtn_password"
+elif [ "$gtn_auth" = "api" ]; then
+    gtn_authstring="-H "X-API-Key: $gtn_apikey"
+else
+    echo "Authentication Method for GlueTun set to an unknown parameter: $gtn_auth"
+    exit 1
+fi
+
+port_number=$(curl --fail --silent --show-error $gtn_authstring $GTN_ADDR/v1/openvpn/portforwarded | jq '.port')
 if [ ! "$port_number" ] || [ "$port_number" = "0" ]; then
     echo "Could not get current forwarded port from gluetun, exiting..."
     exit 1
